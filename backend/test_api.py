8f49f309f30df85774f441a62d152be46c9acd79
Thales <pardinithales@gmail.com>
2024-11-18 10:14:26 -0300

    adicionado último domínio do frontend e do local 3001

--- backend/test_api.py
+++ backend/test_api.py
@@ -33,7 +33,8 @@ origins = [
     "http://localhost:3001",  # Local IP access
     "https://indicadores-upa-frontend-thales-pardinis-projects.vercel.app",  # Production frontend
     "https://indicadores-upa-frontend-2ikhw6iuj-thales-pardinis-projects.vercel.app",  # Alternate production frontend
-    "https://indicadores-upa-frontend-1l4y3uzj9-thales-pardinis-projects.vercel.app"  # Alternate production frontend
+    "https://indicadores-upa-frontend-1l4y3uzj9-thales-pardinis-projects.vercel.app",  # Alternate production frontend
+    "https://indicadores-upa-frontend-cmmrep9h0-thales-pardinis-projects.vercel.app"  # Alternate production frontend
 ]
 
 app.add_middleware(
